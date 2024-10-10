84085d8e2d1a569660a4b56f73f2ccf291c5231f
kb603 <kojoaddobaffour@gmail.com>
2024-10-10 23:16:11 +0000

    setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "../styles/global.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and Share AI prompts",
+};
+
+import { ReactNode } from "react";
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
